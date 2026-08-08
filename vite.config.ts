84d4b6c6483aaccfb83d00c806254e7d917acc16
Peter Mölzer <moelzerpeter@gmail.com>
2026-08-08 10:06:11 +0200

    fix: ignore Cargo build artifacts in Vite

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ export default defineConfig({
   server: {
     port: 5173,
     strictPort: true,
+    watch: {
+      ignored: ["**/src-tauri/target/**"],
+    },
   },
   test: {
     environment: "node",
